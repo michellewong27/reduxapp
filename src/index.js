1204a5350299134cea7b0911715e7d9038634232
Michelle Wong <michellekwong2@gmail.com>
2020-03-02 16:21:59 -0500

    Added store and reducer method

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+//store controls the state
+//it has methods for controlling what goes in & out of state
+    //getState -> returns the state
+    //dispatch -> changes the state
+
+//define default state as its own variable
+const defaultState = {
+    likes: 0,
+    text: "",
+    darkMode: false,
+    things: []
+}
+//createStore requires the reducer function as its argument
+const store = createStore(reducer);
+
+//job of reducer is to return new state
+function reducer(prevState = defaultState, action){
+    console.log("state", prevState)
+    console.log("action", action)
+    //whatever is returned from this function BECOMES your newv state
+        //return an object, (right now ->allows us to always return previous state)
+    return prevState
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
